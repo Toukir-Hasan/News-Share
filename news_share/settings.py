fd41dc2564d98c7ce247d3c20a8f01e4626e29a2
toukir <toukir5@live.com>
2022-11-17 01:45:07 -0500

    original settings file changed and domain name added

--- news_share/settings.py
+++ news_share/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-67c2y=6rc4)p8rlqzt4lal^-!8%ih-u-k#i$9#sv1dq&vbooqb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','newsshare.herokuapp.com/']
 
 
 # Application definition
