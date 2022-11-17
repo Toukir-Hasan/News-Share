dbc547c8268188d442a240e3937de797e5981c18
toukir <toukir5@live.com>
2022-11-17 01:33:40 -0500

    deployment work is done

--- news_share/urls.py
+++ news_share/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import TemplateView
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +28,7 @@ urlpatterns = [
     path('',include("login.urls")),
     path('dashboard/',include("dashboard.urls")),
     # path('results',include("dashboard.urls")),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
